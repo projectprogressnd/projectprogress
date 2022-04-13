1d8a5de5bb387e06a7e148ad4f405401c0b6ede4
projectprogressnd <103526982+projectprogressnd@users.noreply.github.com>
2022-04-14 01:54:52 +0500

    phptocontact

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+
+	$mailto = "office@noyobdunyo.tk";
+	$headers = "From: ".$mailFrom;
+	$txt = "Your message and information is received successfully".$name.".\n\n".$message;
+        
+        mail($mailto, $subject, $txt, $headers);
+        header("Location: https://www.noyobdunyo.uz/contacts.html?mailsend");
+}
